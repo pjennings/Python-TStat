852367e7e76a8f72857168fa54a7f6c8c411f9f6
Paul Jennings <pjennings@pjennings.net>
2011-02-07 07:58:14 -0600

    Working on actual schedule programming.

--- TStatGcal.py
+++ TStatGcal.py
@@ -6,14 +6,14 @@
 #Redistribution and use in source and binary forms, with or without 
 #modification, are permitted provided that the following conditions are met:
 #
-#    * Redistributions of source code must retain the above copyright notice, 
-#      this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in the
-#      documentation and/or other materials provided with the distribution.
-#    * The names of its contributors may not be used to endorse or promote
-#      products derived from this software without specific prior written
-#      permission.
+#	 * Redistributions of source code must retain the above copyright notice, 
+#	   this list of conditions and the following disclaimer.
+#	 * Redistributions in binary form must reproduce the above copyright
+#	   notice, this list of conditions and the following disclaimer in the
+#	   documentation and/or other materials provided with the distribution.
+#	 * The names of its contributors may not be used to endorse or promote
+#	   products derived from this software without specific prior written
+#	   permission.
 
 #THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 #AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
@@ -42,58 +42,58 @@ VERSION = 1.0
 # 2. Go to http://calendar.google.com
 # 3. Create a calendar (called "Thermostat" for example).
 # 4. Add events with titles of the form:
-#    "Heat 70"  -- sets heat to 70 degrees
-#    "Cool 70"  -- sets cool to 70 degrees
-#    "Fan On"   -- forces fan on
-#    "Mode Off" -- forces system off
+#	 "Heat 70"	-- sets heat to 70 degrees
+#	 "Cool 70"	-- sets cool to 70 degrees
+#	 "Fan On"	-- forces fan on
+#	 "Mode Off" -- forces system off
 # 5. Run the following commands (assuming Unix/Linux system):
-#    echo "youraccount@gmail.com" >> ~/.google
-#    echo "yourpassword" >> ~/.google
-#    chmod 400 ~/.google
-#    (where "youraccount@gmail.com" is the account that you created in 
-#     step 1 and "yourpassword" is your password)
+#	 echo "youraccount@gmail.com" >> ~/.google
+#	 echo "yourpassword" >> ~/.google
+#	 chmod 400 ~/.google
+#	 (where "youraccount@gmail.com" is the account that you created in 
+#	  step 1 and "yourpassword" is your password)
 # 6. Add the following to your crontab to run every 5 minutes or so:
-#      TStatGcal.py <thermostat_address> <calendar_name>
-#    Where <thermostat_address> is the IP address of your thermostat 
-#    and <calendar_name> is the name of the calendar you created in 
-#    step 3.
+#	   TStatGcal.py <thermostat_address> <calendar_name>
+#	 Where <thermostat_address> is the IP address of your thermostat 
+#	 and <calendar_name> is the name of the calendar you created in 
+#	 step 3.
 #
 # Notes:
-#    In order to limit the chance that this script sets your 
-#    thermostat to dangerous settings (e.g. too low or off during 
-#    the winter, there are some override variables below:
-#      HEAT_MIN, HEAT_MAX: Minimum/maximum setting for heat
-#      COOL_MIN, COOL_MAX: Minimum/maximum setting for cool
-#      COMMANDS: What parts of the thermostat the script is 
-#                allowed to control
+#	 In order to limit the chance that this script sets your 
+#	 thermostat to dangerous settings (e.g. too low or off during 
+#	 the winter, there are some override variables below:
+#	   HEAT_MIN, HEAT_MAX: Minimum/maximum setting for heat
+#	   COOL_MIN, COOL_MAX: Minimum/maximum setting for cool
+#	   COMMANDS: What parts of the thermostat the script is 
+#				 allowed to control
 #
-#    Set the HEAT/COOL variables to appropriate values for your 
-#    situation.  By default, this script will not set the 
-#    thermostat mode (on/off/auto).  You probably want to leave 
-#    it on auto.  This is to prevent a hacker (or a typo) from 
-#    turning your furnace off during the winter.  
+#	 Set the HEAT/COOL variables to appropriate values for your 
+#	 situation.	 By default, this script will not set the 
+#	 thermostat mode (on/off/auto).	 You probably want to leave 
+#	 it on auto.  This is to prevent a hacker (or a typo) from 
+#	 turning your furnace off during the winter.  
 #
-#    By default, this script does not disable cloud updates. 
-#    That way, if this script does not run for some reason (e.g. 
-#    if your computer crashes), you can still have a reasonable 
-#    backup program running.  When the cloud updates your thermostat,
-#    there may be a short period where the setting does not match
-#    what is on your calendar.  If this behavior is undesirable, you
-#    can disable cloud updates.  
+#	 By default, this script does not disable cloud updates. 
+#	 That way, if this script does not run for some reason (e.g. 
+#	 if your computer crashes), you can still have a reasonable 
+#	 backup program running.  When the cloud updates your thermostat,
+#	 there may be a short period where the setting does not match
+#	 what is on your calendar.	If this behavior is undesirable, you
+#	 can disable cloud updates.	 
 #
-#    At the start time of your event, the script will set the 
-#    the thermostat to the requested setting.  The duration of the 
-#    events on your calendar is ignored.  For example, a simple
-#    program might look like this:
-#      6:30 -- Heat 70
-#      8:00 -- Heat 60
-#     16:00 -- Heat 70
-#     22:00 -- Heat 60
-#    In order to create this program in your calendar, you would need 
-#    four events.  If you create a "Heat 70" event that lasts from 
-#    6:30-22:00 and an overlapping "Heat 60" event that lasts from 
-#    8:00-16:00, you will effectively miss the "Heat 70" command at 
-#    16:00.  Only the start time of the event is used.  
+#	 At the start time of your event, the script will set the 
+#	 the thermostat to the requested setting.  The duration of the 
+#	 events on your calendar is ignored.  For example, a simple
+#	 program might look like this:
+#	   6:30 -- Heat 70
+#	   8:00 -- Heat 60
+#	  16:00 -- Heat 70
+#	  22:00 -- Heat 60
+#	 In order to create this program in your calendar, you would need 
+#	 four events.  If you create a "Heat 70" event that lasts from 
+#	 6:30-22:00 and an overlapping "Heat 60" event that lasts from 
+#	 8:00-16:00, you will effectively miss the "Heat 70" command at 
+#	 16:00.	 Only the start time of the event is used.	
 
 # Minimum and maximum values for heat and cool
 # The script will never set values outside of this range
@@ -108,6 +108,8 @@ COOL_MAX = 100
 # a 'mode off' command and turn your heat off in the winter.
 #COMMANDS = ['Heat', 'Cool', 'Mode', 'Fan']
 COMMANDS = ['Heat', 'Cool', 'Fan']
+
+PERIODS = ['Wake', 'Leave', 'Home', 'Sleep']
 
 try:
   from xml.etree import ElementTree # for Python 2.5 users
@@ -128,6 +130,15 @@ import time
 
 import TStat
 
+def getCalendarService(username, password):
+	# Log in to Google
+	calendar_service = gdata.calendar.service.CalendarService()
+	calendar_service.email = username
+	calendar_service.password = password
+	calendar_service.source = "TStatGCal-%s" % VERSION
+	calendar_service.ProgrammaticLogin()
+	return calendar_service
+
 def main(tstatAddr, commandMap=None, username=None, password=None, calName="Thermostat"):
 	# Connect to thermostat
 	tstat = TStat.TStat(tstatAddr)
@@ -136,18 +147,13 @@ def main(tstatAddr, commandMap=None, username=None, password=None, calName="Ther
 	if commandMap is None:
 		commandMap = {}
 
-	# Log in to Google
-	calendar_service = gdata.calendar.service.CalendarService()
-	calendar_service.email = username
-	calendar_service.password = password
-	calendar_service.source = "TStatGCal-%s" % VERSION
-	calendar_service.ProgrammaticLogin()
+	calendar_service = getCalendarService(username, password)
 
 	# Create date range for event search
 	today = datetime.datetime.today()
 	gmt = time.gmtime()
 	gmtDiff = datetime.datetime(gmt[0], gmt[1], gmt[2], gmt[3], gmt[4]) - today
-	tomorrow = datetime.datetime.today()+datetime.timedelta(days=2)
+	tomorrow = datetime.datetime.today()+datetime.timedelta(days=8)
 
 	query = gdata.calendar.service.CalendarEventQuery()
 	query.start_min = "%04i-%02i-%02i" % (today.year, today.month, today.day)
@@ -174,31 +180,34 @@ def main(tstatAddr, commandMap=None, username=None, password=None, calName="Ther
 	closestEvent = None
 	closestCommand = None
 	closestValue = None
+	periods = {}
 	feed = calendar_service.CalendarQuery(query)
 	for i, an_event in enumerate(feed.entry):
 		#print '\t%s. %s' % (i, an_event.title.text,)
 
 		# Try to map named time period into actual command
 		text = an_event.title.text.strip()
-		if commandMap.has_key(text):
-			text = commandMap[text]
 
-		print "Translated %s into %s" % (an_event.title.text.strip(), text)
-
-		# Skip events that are not valid commands
-		try:
-			(command, value) = text.splitlines()[0].split()
-		except:
-			command = text
-		if command not in COMMANDS:
-			print "Warning: '%s' is not a valid command" % text
-			continue
-		try:
-			float(value)
-		except:
-			if value not in ['Off', 'On', 'Auto']:
-				print "Warning: '%s' is not a valid command" % an_event.title.text
+		if not text in PERIODS:
+			if commandMap.has_key(text):
+				text = commandMap[text]
+	
+			print "Translated %s into %s" % (an_event.title.text.strip(), text)
+	
+			# Skip events that are not valid commands
+			try:
+				(command, value) = text.splitlines()[0].split()
+			except:
+				command = text
+			if command not in COMMANDS:
+				print "Warning: '%s' is not a valid command" % text
 				continue
+			try:
+				float(value)
+			except:
+				if value not in ['Off', 'On', 'Auto']:
+					print "Warning: '%s' is not a valid command" % an_event.title.text
+					continue
 		for a_when in an_event.when:
 			d = a_when.start_time.split("T")[0]
 			t = a_when.start_time.split("T")[1].split(".")[0]
@@ -209,27 +218,50 @@ def main(tstatAddr, commandMap=None, username=None, password=None, calName="Ther
 			d = dt-datetime.datetime.today()
 			#print "d.days:", d.days
 
-			# Skip events that are in the future
-			if d.days >= 0:
-				continue
-
-			if closest is None:
-				closest = d
-				closestDT = dt
-				closestWhen = a_when
-				closestEvent = an_event
-				closestCommand = command
-				closestValue = value
+			if text in PERIODS:
+			    if not periods.has_key(dt.day):
+			        periods[dt.day] = {}
+				periods[dt.day][text] = dt
 			else:
-				if d.days < closest.days:
+				# Skip events that are in the future
+				if d.days >= 0:
 					continue
-				if d.seconds > closest.seconds:
+	
+				if closest is None:
 					closest = d
 					closestDT = dt
 					closestWhen = a_when
 					closestEvent = an_event
 					closestCommand = command
 					closestValue = value
+				else:
+					if d.days < closest.days:
+						continue
+					if d.seconds > closest.seconds:
+						closest = d
+						closestDT = dt
+						closestWhen = a_when
+						closestEvent = an_event
+						closestCommand = command
+						closestValue = value
+
+	print "Found periods:", periods
+	
+	# Handle programmed periods
+	periodCommands = {}
+	for day in range(0,7):
+	    if not periodCommands.has_key(day):
+            periodCommands[day] = []
+        for p in PERIODS:
+    
+            if periods.has_key(p):
+                periodCommands.append(int(periods[p].hour*60+periods[p].minute))
+                periodCommands.append(int(commandMap[p].split()[-1]))
+            else:
+                periodCommands.append(periodCommands[-2])
+                periodCommands.append(periodCommands[-2])			
+
+	print "Commands:", periodCommands
 
 	if closestEvent is None:
 		print "No events found"
